d00dfa145d7dfb44291035a2011bdb2fb0258523
Audrey Livirya <aliv074@aucklanduni.ac.nz>
2019-05-27 21:45:26 +1200

    Make app cover entire page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/xq-light.css'
 import './index.css';
 import App from './App';
 import CodeMirror from 'codemirror'
@@ -8,6 +9,8 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 let editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
     lineNumbers: true,
+    indentUnit: 4,
+    theme: "xq-light"
 })
 editor.refresh()
 // If you want your app to work offline and load faster, you can change
