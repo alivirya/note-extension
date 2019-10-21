cfb86b61e31f8a5c76ada2f1dfb0a7e4e250e285
Audrey Livirya <aliv074@aucklanduni.ac.nz>
2019-10-21 13:28:06 +1300

    Updated packages hopefully fixes dependency issues

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/base16-light.css'
+import 'codemirror/theme/pastel-on-dark.css'
 import "codemirror/mode/markdown/markdown.js"
 import './index.css';
 import App from './App';
 import CodeMirror from 'codemirror'
 import * as serviceWorker from './serviceWorker';
-ReactDOM.render( < App / > , document.getElementById('root'));
+ReactDOM.render( <App /> , document.getElementById('root'));
 let editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
     lineNumbers: true,
     indentUnit: 4,
-    theme: "base16-light",
+    theme: "pastel-on-dark",
     mode: "markdown"
 })
 editor.refresh()
